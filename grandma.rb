da102d805c60b42988b850f57531a3d402c66247
Alyssa Brown <alyssa.brown@live.com>
2018-12-18 18:06:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 speak_to_grandma= "I love you grandma!".upcase
-  if speak_to_grandma== "I love you grandma"
-    puts "Huh?! Speak up, Sonny!"
-  elseif speak_to_grandma== "I love you grandma!".upcase
-    puts "I love you too pumpkin!"
+  if speak_to_grandma== "I love you grandma".upcase
+    puts '"I love you too pumpkin!'
+  elseif speak_to_grandma== "I love you grandma!"
+    puts "Huh?! Speak up, sonny!"
   else speak_to_grandma== "I love you grandma!".upcase
     puts "No, not since 1938!"
   end
