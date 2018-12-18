17da0c4bf141b344d011638a60afb5dda7a0bd18
Alyssa Brown <alyssa.brown@live.com>
2018-12-18 17:58:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,7 @@ speak_to_grandma= "I love you grandma!.upcase"
     puts "I love you too pumpkin!"
   else speak_to_grandma== "I love you grandma!.upcase"
     puts "No, not since 1938!"
+  end
     
 
 # Whatever you say to grandma, she should respond with
