e39f4fe7daf7e4eee91db9ba8117efff089a2871
Alyssa Brown <alyssa.brown@live.com>
2018-12-18 18:11:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def
   speak_to_grandma= ("I love you grandma!".upcase)
     if speak_to_grandma== "I love you grandma".upcase
     puts '"I love you too pumpkin!'
-  elseif speak_to_grandma== "I love you grandma!"
+  elseif speak_to_grandma== "Hi nana how are you!"
     puts "Huh?! Speak up, sonny!"
   else speak_to_grandma== "I love you grandma!".upcase
     puts "No, not since 1938!"
